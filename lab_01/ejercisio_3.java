8161fa556f0d3983651def21955bb0336d148ece
rwciANA <145408428+rwciANA@users.noreply.github.com>
2025-05-08 09:39:45 -0500

    Update ejer 3

--- lab_01/ejercisio_3.java
+++ lab_01/ejercisio_3.java
@@ -1,1 +1,25 @@
+import java.util.Arrays;
+
+public class OrdenamientoInsercion {
+    public static void main(String[] args) {
+        int[] arreglo = {7, 3, 9, 1, 5};
+
+        System.out.println("Arreglo original: " + Arrays.toString(arreglo));
+        insercion(arreglo);
+        System.out.println("Arreglo ordenado: " + Arrays.toString(arreglo));
+    }
+
+    static void insercion(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int actual = arr[i];
+            int j = i - 1;
+           
+            while (j >= 0 && arr[j] > actual) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = actual;
+        }
+    }
+}
 
