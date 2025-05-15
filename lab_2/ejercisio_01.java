358e0218cc02558ef7f0e32792dd01836c7d9357
rwciANA <rcondorii@unsa.edu.pe>
2025-05-15 15:20:10 -0500

    resolviendo

--- lab_2/ejercisio_01.java
+++ lab_2/ejercisio_01.java
@@ -1,6 +1,6 @@
 
 package lab_2;
-class ejercisio_1 {
+class ejercisio_01 {
 
     void  repetir(){
         repetir();
@@ -8,7 +8,7 @@ class ejercisio_1 {
 
     public static void main(String[] args) {
         System.err.println("hola");
-    ejercisio_1 re= new  ejercisio_1();
+    ejercisio_01 re= new  ejercisio_01();
         re.repetir();
     }
 }
---
+++ lab_2/ejercisios_propuestos/problem_1.java
@@ -0,0 +1,31 @@
+package lab_2.ejercisios_propuestos;
+
+
+public class problem_1 {
+
+         public static void main(String[] args) {
+
+        int [] array= {3,4,56,3,34,66,67,7,3};
+        
+        int [] c =invertir( array);
+
+        for (Object elem : c) {
+            System.out.println("ffff" +c);
+        }
+        
+    }
+    
+    public static int [] invertir (int [] a){
+
+        int []b  = new int[a.length];
+        for(int idx =a.length-1 ,i=0;  i< a.length &&idx >=0 ; i++,idx--){
+            
+            b[i]=a[idx];
+
+        }
+
+        
+        return b;
+
+    }
+}
