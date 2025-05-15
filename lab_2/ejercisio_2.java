a9f023b6970ad73c139d887b8c84bc043340f781
rwciANA <rcondorii@unsa.edu.pe>
2025-05-15 14:32:56 -0500

    ejer 2 resolviendo

--- lab_2/ejercisio_2.java
+++ lab_2/ejercisio_2.java
@@ -2,7 +2,13 @@ package lab_2;
 
 public class ejercisio_2 {
 
+    void imprimir (int x){
+        System.out.println(x);
+        imprimir(x-1);
+    }
     public static void main(String[] args) {
+        ejercisio_2 re =new  ejercisio_2();
+        re.imprimir(5);
         
     }
 }
